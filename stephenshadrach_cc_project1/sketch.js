57d8cfa85335286e9d92f81634039d3801ab49bf
stephen shadrach <stephenlgz@gmail.com>
2021-03-18 14:46:53 -0400

    third push
    got never-ending stars to move

--- stephenshadrach_cc_project1/sketch.js
+++ stephenshadrach_cc_project1/sketch.js
@@ -1,54 +1,48 @@
 /* adjective: reflective
 
-  1. space background constantly moving, start slow and speed up movement
-  2. when the sun appears near the balls, shapes that represent thoughts are reflected inside them and move around randomly
-    (i.e. ":)", ":(", "?", "!", "<3")
+  when the sun appears near the crystalBalls, shapes that represent thoughts are reflected inside them and move around randomly
+  (i.e. ":)", ":(", "?", "!", "<3")
 
   no user input (based off of time)
-  
+
 */
 
 
-//create array for CrystalBall objects
-
-let x;
-let y;
-let stars;
+let crystalBalls = []
+let stars = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
-
-  x = new Array(100);
-  y = new Array(100);
-  stars = new Array(100);
-
-  for(let i = 0; i < 100; i++) { // creates random x and y locations for potential stars
-    x[i] = random(0, width);
-    y[i] = random(0, height);
-  }
-
-  for(let j = 0; j < 100; j++){ // loads stars array with stars at random places
-    stars[j] = new Star(x[j], y[j]);
+  for (let i = 0; i < 100; i++) {
+    stars[i] = new Star();
   }
 }
 
 function draw() {
-  for(let j = 0; j < 100; j++){ // displays stars
-    stars[j].display();
+  background(0);
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].display();
+    stars[i].move();
   }
 }
 
 
 class Star { // creates a new star object
-  constructor(x, y){
-    this.xLoc = x;
-    this.yLoc = y;
+  constructor(){
+    this.pos = createVector(random(width), random(height));
+    this.vel = createVector(.5, 0);
   }
 
-  display(){
+  display(){ // displays each star
     stroke(255);
     strokeWeight(5);
-    point(this.xLoc, this.yLoc);
+    point(this.pos.x, this.pos.y);
+  }
+
+  move(){ // moves each star
+    this.pos.add(this.vel);
+    if (this.pos.x > width) {
+      this.pos.x = random(-10, -10);
+    }
   }
 }
