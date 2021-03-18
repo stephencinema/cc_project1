9239d1cc1d937c77bf42c61184921670d9a08097
stephen shadrach <stephenlgz@gmail.com>
2021-03-18 14:57:35 -0400

    fourth push
    more organization

--- stephenshadrach_cc_project1/sketch.js
+++ stephenshadrach_cc_project1/sketch.js
@@ -25,24 +25,3 @@ function draw() {
     stars[i].move();
   }
 }
-
-
-class Star { // creates a new star object
-  constructor(){
-    this.pos = createVector(random(width), random(height));
-    this.vel = createVector(.5, 0);
-  }
-
-  display(){ // displays each star
-    stroke(255);
-    strokeWeight(5);
-    point(this.pos.x, this.pos.y);
-  }
-
-  move(){ // moves each star
-    this.pos.add(this.vel);
-    if (this.pos.x > width) {
-      this.pos.x = random(-10, -10);
-    }
-  }
-}
