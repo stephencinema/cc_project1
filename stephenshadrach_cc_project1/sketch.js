2c9508a6659bbc3fbd618e219a3d3ffd8448f069
stephen shadrach <stephenlgz@gmail.com>
2021-03-17 22:48:50 -0400

    second push
    created star background

--- stephenshadrach_cc_project1/sketch.js
+++ stephenshadrach_cc_project1/sketch.js
@@ -1,8 +1,54 @@
+/* adjective: reflective
+
+  1. space background constantly moving, start slow and speed up movement
+  2. when the sun appears near the balls, shapes that represent thoughts are reflected inside them and move around randomly
+    (i.e. ":)", ":(", "?", "!", "<3")
+
+  no user input (based off of time)
+  
+*/
+
+
+//create array for CrystalBall objects
+
+let x;
+let y;
+let stars;
+
 function setup() {
-  createCanvas(400,400);
-  background(220);
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+
+  x = new Array(100);
+  y = new Array(100);
+  stars = new Array(100);
+
+  for(let i = 0; i < 100; i++) { // creates random x and y locations for potential stars
+    x[i] = random(0, width);
+    y[i] = random(0, height);
+  }
+
+  for(let j = 0; j < 100; j++){ // loads stars array with stars at random places
+    stars[j] = new Star(x[j], y[j]);
+  }
 }
 
 function draw() {
-  ellipse(width/2, height/2, 10,10);
+  for(let j = 0; j < 100; j++){ // displays stars
+    stars[j].display();
+  }
+}
+
+
+class Star { // creates a new star object
+  constructor(x, y){
+    this.xLoc = x;
+    this.yLoc = y;
+  }
+
+  display(){
+    stroke(255);
+    strokeWeight(5);
+    point(this.xLoc, this.yLoc);
+  }
 }
