822775f5b1bb5057666567e52dcb3a321d8e26de
stephen shadrach <stephenlgz@gmail.com>
2021-03-18 15:25:26 -0400

    fifth push
    a little change in direction, more coherent.

--- stephenshadrach_cc_project1/sketch.js
+++ stephenshadrach_cc_project1/sketch.js
@@ -1,14 +1,12 @@
 /* adjective: reflective
 
-  when the sun appears near the crystalBalls, shapes that represent thoughts are reflected inside them and move around randomly
-  (i.e. ":)", ":(", "?", "!", "<3")
-
-  no user input (based off of time)
+  1. when the sun appears near the planets, lines that represent light are reflected from them and move around randomly
+  2. the planets will also light up
 
 */
 
 
-let crystalBalls = []
+let planets = [];
 let stars = [];
 
 function setup() {
